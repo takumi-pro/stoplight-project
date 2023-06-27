a0f3e1bb64c5a4b8dd94597d992454e3df8d8855
takumi <takumidiary.0927@gmail.com>
2023-06-27 19:44:19 +0900

    feature: todoアプリケーションAPI仕様書

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODAyNw/spectral.js?branch_slug=master&token=27ba2385-5668-47bf-bc95-741b66b9e17d";
+export default { extends: ruleset };
